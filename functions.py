4f48ecda4e3a9d5e10f8efc3ccb43ced54639ec4
ebobola-dev <romawot94@gmail.com>
2024-04-24 23:54:59 +0300

    program is done

--- functions.py
+++ functions.py
@@ -1,4 +1,31 @@
 from models.order import Order
 
-def get_new_year_eve_orders(orders: tuple[Order]):
-	pass
+def get_new_year_eve_orders(orders: tuple[Order]) -> tuple[Order]:
+	return tuple(filter(lambda order: order.ordered_at.month == 12, orders))
+
+def get_category_statistics(orders: tuple[Order]) -> dict:
+	categories = {}
+	for order in orders:
+		for product in order.items:
+			if product.category.name in categories:
+				categories[product.category.name] += 1
+			else:
+				categories[product.category.name] = 1
+	return categories
+
+def get_most_popular_categories(category_statistics: dict) -> tuple[str]:
+	max_category_usage = max(category_statistics.values())
+
+	most_popular_categories = tuple(sorted(category[0] for category in filter(lambda category_statistic: category_statistic[1] == max_category_usage, category_statistics.items())))
+	"""
+				↑↑↑
+	most_popular_categories = []
+	for key, value in category_statistics.items():
+		if value == max_category_usage:
+			most_popular_categories.append(key)
+
+	most_popular_categories = sorted(most_popular_categories)
+	most_popular_categories = tuple(most_popular_categories)
+	"""
+
+	return most_popular_categories
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from utils.json_util import JsonUtils
 from models.order import Order
 from functions import *
 
+print()
 try:
 	source_orders_json = JsonUtils.read(SOURCE_DATA_FILEPATH)
 except:
@@ -11,5 +12,20 @@ except:
 	quit()
 
 orders = tuple(Order.from_json(json_order) for json_order in source_orders_json)
+print(f'Файл "{SOURCE_DATA_FILEPATH}" успешно прочитан, получено заказов: {len(orders)}')
 
-print()
+new_year_eve_orders = get_new_year_eve_orders(orders)
+if len(new_year_eve_orders) == 0:
+	print('\nВ канун нового года ничего не заказывали..(')
+	quit()
+print(f'\nЗаказов в канун нового года: {len(new_year_eve_orders)}')
+
+category_statistics = get_category_statistics(new_year_eve_orders)
+print('\nСтатистика по категориям заказов:')
+for key, value in category_statistics.items():
+	print(f'\t{key} - {value} зз')
+
+most_popular_categories = get_most_popular_categories(category_statistics)
+print(f'\nНаиболее популярные категории: {', '.join(most_popular_categories)}')
+
+JsonUtils.write('result.json', {'categories': most_popular_categories})
